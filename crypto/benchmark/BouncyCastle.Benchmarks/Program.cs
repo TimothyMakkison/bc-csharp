fcf32a82f24a6dc168d728d5bffd664bf08ff3a9
Timothy Makkison <timmakkison@gmail.com>
2022-11-20 18:23:56 +0000

    Add Blake2s and Blake2xs intrinsics

--- crypto/benchmark/BouncyCastle.Benchmarks/Program.cs
+++ crypto/benchmark/BouncyCastle.Benchmarks/Program.cs
@@ -3,14 +3,14 @@ using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Running;
 using Org.BouncyCastle.Crypto.Digests;
 
-BenchmarkRunner.Run<Blake2bDigestBenchmark>();
+BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
 
 [MemoryDiagnoser]
 [SimpleJob(RuntimeMoniker.Net60)]
 [HtmlExporter]
 public class Blake2bDigestBenchmark
 {
-    private readonly Blake2bDigest _blake2b = new Blake2bDigest();
+    private readonly Blake2bDigest _blake2 = new ();
     private byte[] _data = Array.Empty<byte>();
 
     [GlobalSetup]
@@ -31,9 +31,76 @@ public class Blake2bDigestBenchmark
     [Benchmark]
     public byte[] GetHash()
     {
-        _blake2b.BlockUpdate(_data);
+        _blake2.BlockUpdate(_data);
         var digest = new byte[64];
-        _blake2b.DoFinal(digest);
+        _blake2.DoFinal(digest);
         return digest;
     }
 }
+
+[MemoryDiagnoser]
+[SimpleJob(RuntimeMoniker.Net60)]
+[HtmlExporter]
+public class Blake2sDigestBenchmark
+{
+    private readonly Blake2sDigest _blake2 = new();
+    private byte[] _data = Array.Empty<byte>();
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        Random rnd = new Random(42);
+        _data = new byte[Size];
+        rnd.NextBytes(_data);
+    }
+
+    [Params((int)3,
+   (int)1E+2,  // 100 bytes
+   (int)1E+3,  // 1 000 bytes = 1 KB
+   (int)1E+4  // 10 000 bytes = 10 KB
+   )]
+    public int Size { get; set; }
+
+    [Benchmark]
+    public byte[] GetHash()
+    {
+        _blake2.BlockUpdate(_data);
+        var digest = new byte[64];
+        _blake2.DoFinal(digest);
+        return digest;
+    }
+}
+
+[MemoryDiagnoser]
+[SimpleJob(RuntimeMoniker.Net60)]
+[HtmlExporter]
+public class Blake2xsDigestBenchmark
+{
+    private readonly Blake2xsDigest _blake2 = new(16);
+    private byte[] _data = Array.Empty<byte>();
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        Random rnd = new Random(42);
+        _data = new byte[Size];
+        rnd.NextBytes(_data);
+    }
+
+    [Params((int)3,
+   (int)1E+2,  // 100 bytes
+   (int)1E+3,  // 1 000 bytes = 1 KB
+   (int)1E+4  // 10 000 bytes = 10 KB
+   )]
+    public int Size { get; set; }
+
+    [Benchmark]
+    public byte[] GetHash()
+    {
+        _blake2.BlockUpdate(_data);
+        var digest = new byte[16];
+        _blake2.DoFinal(digest);
+        return digest;
+    }
+}
+
